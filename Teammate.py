6073750bc34dfeee2a6039435b4091be3ea3a827
Toly-Leto <anatolii_stankovskiii@mail.ru>
2025-11-22 22:15:16 +0600

    one bag hase been fixed

--- Teammate.py
+++ Teammate.py
@@ -17,11 +17,11 @@ class Teammate:
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
-        if self.rect.right > screen_w - paddle_w * 5: self.speed_x *= -1
-        if self.rect.left < paddle_w * 5: self.speed_x *= -1
+        if self.rect.right > screen_w - paddle_w * 2: self.speed_x *= -1
+        if self.rect.left < paddle_w * 2: self.speed_x *= -1
 
-        if self.rect.top < paddle_w: self.speed_y *= -1
-        if self.rect.bottom > screen_h - paddle_w: self.speed_y *= -1
+        if self.rect.top < 0: self.speed_y *= -1
+        if self.rect.bottom > screen_h: self.speed_y *= -1
 
     def draw(self, screen):
         self.move()
